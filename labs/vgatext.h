e3fe02cbe07ac708ea1d3f6a951c44366ee5ce0b
Sorav Bansal <sbansal@cse.iitd.ernet.in>
2017-01-03 18:57:03 +0530

    MMIO vgatext

---
+++ labs/vgatext.h
@@ -0,0 +1,9 @@
+#include "util/io.h"
+
+namespace vgatext{
+
+   static inline void writechar(int loc, uint8_t c, uint8_t bg, uint8_t fg, addr_t base){
+     //your code goes here
+   }
+
+}//namespace vgatext
--- x86/main.cc
+++ x86/main.cc
@@ -1,5 +1,15 @@
+#include "labs/vgatext.h"
 
+
+//
+// main
+//
 extern "C" void core_boot(){
+  addr_t vgatext_base=(addr_t)0xb8000;
 
+  const char* p="Hello, world!";
+  for(int loc=0;*p;loc++,p++){
+    vgatext::writechar(loc,*p,0,7,vgatext_base);
+  }
 }
 
