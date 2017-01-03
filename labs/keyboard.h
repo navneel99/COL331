63a49e536bba2b126f9436d52c04532307dfd88b
Sorav Bansal <sbansal@cse.iitd.ernet.in>
2017-01-03 19:40:50 +0530

    Abstract mmio/pmio : keyboard

---
+++ labs/keyboard.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "generated/lpc_kbd.dev.h"
+
+namespace lpc_kbd{
+
+  static inline bool has_key(lpc_kbd_t& dev){
+
+    //insert your code here
+  }
+
+  static inline uint8_t get_key(lpc_kbd_t& dev){
+
+    //insert your code here
+  }
+
+}// namespace lpc_kbd
+
