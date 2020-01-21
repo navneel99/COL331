1dbe49ab038cd094a9b479a6e151ed1297a1ec13
Navneel Mandal <navneel99@gmail.com>
2020-01-22 01:22:44 +0530

    Removed Unneccesary line

--- labs/vgatext.h
+++ labs/vgatext.h
@@ -3,7 +3,7 @@
 namespace vgatext{
 
    static inline void writechar(int loc, uint8_t c, uint8_t bg, uint8_t fg, addr_t base){
-	mmio::read16(base,2*loc);
+	//mmio::read16(base,2*loc);
 	mmio::write8(base,2*loc,c);
 	mmio::write8(base,2*loc+1,(bg<<4)+fg);
    }
