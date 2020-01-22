0e3dad389476312b6c776b75efcbc69aa85dde3a
Navneel Mandal <navneel99@gmail.com>
2020-01-23 01:22:50 +0530

    1.2 finished

--- labs/serial.h
+++ labs/serial.h
@@ -5,10 +5,17 @@
 namespace serial{
 
   static inline bool is_transmitter_ready(io_t baseport){
+	uint8_t a = io::read8(baseport,5) ;
+	if (a & (1<<5) == 32){
+		return true;
+	}
+	return false;
+
     //insert your code here
   }
 
   static inline void writechar(uint8_t c, io_t baseport){
+	io::write8(baseport,0,c);
     //insert your code here
   }
 
