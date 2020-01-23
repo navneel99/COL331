a3116d2c33a18944beda207b06793779de364db2
Navneel Mandal <navneel99@gmail.com>
2020-01-23 22:41:46 +0530

    Corrected and minified codes of 1.2 and 1.3

--- labs/keyboard.h
+++ labs/keyboard.h
@@ -6,18 +6,11 @@ namespace lpc_kbd{
 
   static inline bool has_key(lpc_kbd_t& dev){
 	lpc_kbd_status_t rv = lpc_kbd_status_rd(&dev);
-	uint8_t fv = lpc_kbd_status_obf_extract(rv);
-	if (fv == 1){
-	return true;
-	}
-	return false;
-    //insert your code here
+	return lpc_kbd_status_obf_extract(rv);
   }
 
   static inline uint8_t get_key(lpc_kbd_t& dev){
 	return lpc_kbd_input_rd(&dev);
-
-    //insert your code here
   }
 
 }// namespace lpc_kbd
--- labs/serial.h
+++ labs/serial.h
@@ -5,18 +5,11 @@
 namespace serial{
 
   static inline bool is_transmitter_ready(io_t baseport){
-	uint8_t a = io::read8(baseport,5) ;
-	if (a & (1<<5) == 32){
-		return true;
-	}
-	return false;
-
-    //insert your code here
+  return (io::read8(baseport,5) & 0b00100000);
   }
 
   static inline void writechar(uint8_t c, io_t baseport){
 	io::write8(baseport,0,c);
-    //insert your code here
   }
 
 } //end serial
